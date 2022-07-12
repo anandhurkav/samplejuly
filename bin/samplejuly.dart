57bfe6d5feb4da6d4beca1399c1b7129ac3e2e77
Acer <anandhuradhakrishnanav@gmail.com>
2022-07-12 21:35:44 +0530

    Initial commit

---
+++ bin/samplejuly.dart
@@ -0,0 +1,5 @@
+import 'package:samplejuly/samplejuly.dart' as samplejuly;
+
+void main(List<String> arguments) {
+  print('Hello world: ${samplejuly.calculate()}!');
+}
---
+++ test/samplejuly_test.dart
@@ -0,0 +1,8 @@
+import 'package:samplejuly/samplejuly.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
