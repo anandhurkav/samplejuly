49a401808e70a07948febd64db954123006b4c2f
Acer <anandhuradhakrishnanav@gmail.com>
2022-08-03 10:39:14 +0530

    commit

---
+++ lib/evennumbers.dart
@@ -0,0 +1,10 @@
+void main()
+{
+  var list = [1,2,3,4,5,6,7,8,9,10];
+  for(int i=1; i <= list.length; i++)
+  {
+    if(i % 2 == 0){
+      print(i);
+    }
+  }
+}
---
+++ lib/sumoftennaturalnumber.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  var list = [1,2,3,4,5,6,7,8,9,10];
+  int sum=0;
+  for(int i=1; i <= list.length; i++)
+  {
+
+    sum = sum+i;
+    }
+  print(sum);
+
+}
